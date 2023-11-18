8279cc780ef0438793a17e49784f037e2ba66e74
NikitaSamoylov <nsam7655@gmail.com>
2023-11-18 22:13:20 +0300

    init: add basic project structure

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          initialproj
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
