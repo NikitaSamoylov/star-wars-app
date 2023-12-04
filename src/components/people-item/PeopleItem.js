6b1acf321c1dfde7022c273b354901f8df4a9639
unknown <nsam7655@gmail.com>
2023-12-04 16:25:40 +0300

    feat: turn PeopleItem into functional component

--- src/components/people-item/PeopleItem.js
+++ src/components/people-item/PeopleItem.js
@@ -1,24 +1,19 @@
-import { Component } from 'react';
-
 import './people-item.scss';
 
-class PeopleItem extends Component {
+const PeopleItem = (props) => {
+    const {image, name, clickItem, isActive} = props;
+    const peopleItemClass = isActive
+                                    ? "people-list__item people-item clicked-item"
+                                    : "people-list__item people-item"
 
-    render() {
-        const {image, name, clickItem, isActive} = this.props;
-        const peopleItemClass = isActive
-                                        ? "people-list__item people-item clicked-item"
-                                        : "people-list__item people-item"
-
-        return (
-            <>
-             <li className={peopleItemClass} onClick={clickItem} tabIndex={5}>
-                <img className="people-item__img" src={image} alt="avatar"/>
-                <h2 className="people-item__name">{name}</h2>
-             </li>
-            </>
-        )
-    }
+    return (
+        <>
+            <li className={peopleItemClass} onClick={clickItem} tabIndex={5}>
+            <img className="people-item__img" src={image} alt="avatar"/>
+            <h2 className="people-item__name">{name}</h2>
+            </li>
+        </>
+    )
 }
 
 export default PeopleItem;
