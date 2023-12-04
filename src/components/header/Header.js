3a9170ad38baf4e6e49c5b9bc82a9b36ed010230
unknown <nsam7655@gmail.com>
2023-12-04 16:33:40 +0300

    feat: turn Header into functional component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './header.scss';
 import '../../resources/img/swlogo.png';
 
-class Header extends Component {
-    state = {
-        headerClass: 'header',
-      }
+const Header = () =>  {
+    const [headerClass, setHeaderClass] = useState('header');
 
-    listenScrollEvent = (e) => {
+    const listenScrollEvent = (e) => {
         if (window.scrollY > 40) {
-            this.setState({headerClass: 'header header--scroll'})
+            setHeaderClass('header header--scroll')
         } else {
-            this.setState({headerClass: 'header'})
+            setHeaderClass('header')
         }
     }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.listenScrollEvent)
-    }
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+        return (() => {
+            window.removeEventListener('scroll', listenScrollEvent);
+        })
+    }, [])
 
-    render () {
-        return (
-            <header className={this.state.headerClass}>
-                <div className="container header__container">
-                    <a href="/"><img
-                        src={require('../../resources/img/swlogo.png')}
-                        alt="logo"
-                        tabIndex={1}/></a>
-                    <ul className="nav">
-                        <li className="nav__item nav__item--active"
-                            tabIndex={2}>Characters</li>
-                        <li className="nav__item"
-                            tabIndex={3}>Films</li>
-                        <li className="nav__item"
-                            tabIndex={4}>Starships</li>
-                    </ul>
-                </div>
-            </header>
-        )
-    }
+    return (
+        <header className={headerClass}>
+            <div className="container header__container">
+                <a href="/"><img
+                    src={require('../../resources/img/swlogo.png')}
+                    alt="logo"
+                    tabIndex={1}/></a>
+                <ul className="nav">
+                    <li className="nav__item nav__item--active"
+                        tabIndex={2}>Characters</li>
+                    <li className="nav__item"
+                        tabIndex={3}>Films</li>
+                    <li className="nav__item"
+                        tabIndex={4}>Starships</li>
+                </ul>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
